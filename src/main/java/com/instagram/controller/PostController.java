705cf296ac0b12baee07ddcef5cded8abce0a08a
dhiraj <dhiraj7219@gmail.com>
2023-01-15 20:34:10 +0530

    update POST-Controller

--- src/main/java/com/instagram/controller/PostController.java
+++ src/main/java/com/instagram/controller/PostController.java
@@ -1,5 +1,16 @@
 package com.instagram.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.instagram.service.PostService;
+
+@RestController
+@RequestMapping("/api/v1/posts")
 public class PostController {
+
+	@Autowired
+	private PostService postService;
 
 }
