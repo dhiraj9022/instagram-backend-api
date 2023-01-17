e7f2c393b74877114085dae374b0d634dd63db4b
dhiraj <dhiraj7219@gmail.com>
2023-01-17 17:57:14 +0530

    repository updated

--- src/main/java/com/instagram/repo/CommentRepository.java
+++ src/main/java/com/instagram/repo/CommentRepository.java
@@ -1,15 +1,11 @@
 package com.instagram.repo;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.instagram.model.Comment;
-import com.instagram.model.Post;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
-	List<Comment> findAllByPost(Post post);
 }
--- src/main/java/com/instagram/repo/UserRepository.java
+++ src/main/java/com/instagram/repo/UserRepository.java
@@ -1,5 +1,7 @@
 package com.instagram.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.instagram.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+	Optional<User> findByUsername(String username);
 
 }
